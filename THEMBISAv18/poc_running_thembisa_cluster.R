4cf548a6badfb7ff59ce7232bf5b6bc97d0a262c
Stefan Rautenbach <48711765+StefanRautenbach@users.noreply.github.com>
2023-02-08 14:44:27 +0000

    Updated cluster workflow
    Moved all operations to within THEMBISAv18 then did the following:
    • modify_rollout.R -> modify_rollout_cluster.R in THEMBISAv18
    o	Remove here()
    o	Remove “THEMBISAv18/” from all filepaths
    • support_modify_inputs.R -> support_modify_inputs_cluster.R in THEMBISAv18
    o	No other changes required
    •	read_and_run -> read_and_run_cluster.R in THEMBISAv18
    o	remove library(here)
    o	remove other uses of here() and THEMBISAv18
    o	update run_thembisa() to only include the system() commands
    •	modify_rollout.R -> modify_rollout_cluster.R in THEMBISAv18
    o	Remove here()
    o	Remove “THEMBISAv18/” from all filepaths
    •	support_modify_inputs.R -> support_modify_inputs_cluster.R in THEMBISAv18
    o	No other changes required
    • cluster_function.R -> cluster_function_thembisav18.R
    o	Removed library(here)
    o	Removed all here() and THEMBISAv18/

---
+++ THEMBISAv18/poc_running_thembisa_cluster.R
@@ -0,0 +1,86 @@
+workdir <- "Q:/Git/HIV_EndGame_SA/THEMBISAv18" 
+setwd(workdir)
+library(didehpc)
+getwd()
+
+mrc_config <- didehpc::didehpc_config(credentials = "spr21", workdir=workdir)
+packages = c("tidyr", "dplyr","ggplot2", "readr", "purrr")
+sources <- c("modify_rollout_cluster.R", "read_and_run_cluster.R", "support_modify_inputs_cluster.R", "cluster_function_thembisav18.R")
+ctx <- context::context_save("contexts", packages = packages, sources = sources)
+mrcq <- didehpc::queue_didehpc(ctx, config=mrc_config)
+h <- mrcq$enqueue(packageVersion("ggplot2"))
+h$wait(10)
+# j <- mrcq$enqueue(system("g++ -std=c++14 THEMBISA.cpp StatFunctions.cpp mersenne.cpp -o thembisa"))
+# j$status()
+# j$log()
+# 
+k <- mrcq$enqueue(system("./thembisa"))
+k$status()
+k$log()
+# 
+# x<- mrcq$enqueue(run_thembisa())
+# x$status()
+# x$log()
+
+pitc_reduction_years<- c(2025)
+pitc_reduction_percentage <- c(0)
+condom_usage_reduction <- FALSE
+fsw_condom_usage_decrease <- 0
+st_condom_usage_decrease <- 0
+lt_condom_usage_decrease <- 0
+condom_incr_start <- 2025
+art_coverage_increase <- FALSE
+art_interrupt_rate_decrease <- 0 
+art_incr_start <- 2025
+art_coverage_decrease <- FALSE
+art_interrupt_rate_increase <- 0
+art_decr_start <- 2025
+cumulative_years <- 50
+summary_name <- "summary_concept"
+
+summary_test <- read.csv("results/summary_test.csv")
+
+pars <- expand_grid(pitc_reduction_years, 
+                    pitc_reduction_percentage, 
+                    condom_usage_reduction,
+                    fsw_condom_usage_decrease, 
+                    st_condom_usage_decrease, 
+                    lt_condom_usage_decrease,
+                    condom_incr_start, 
+                    art_coverage_increase,
+                    art_interrupt_rate_decrease,
+                    art_incr_start,
+                    art_coverage_decrease,
+                    art_interrupt_rate_increase,
+                    art_decr_start,
+                    cumulative_years,
+                    summary_name)
+
+for (i in 1:length(pars$summary_name)){
+  name <- paste0("summary_name", i)
+  pars$summary_name[i] <- name
+} 
+
+x <- mrcq$enqueue_bulk(pars, run_on_cluster) 
+x$status()
+
+filepaths <- paste0("results/", pars$summary_name, ".csv")
+temp <- lapply(filepaths, read.csv)
+names(temp) <- seq(1:length(pars$summary_name))
+combined_summary <- bind_rows(temp, .id = "scenario_number")
+
+combined_summary %>% mutate(intervention_year = as.factor(pitc_reduction_year)) %>% 
+  filter(pitc_reduction_year == 2025, indicator == "TotalHIVtests", year >= 2020) %>% 
+  ggplot(aes(year, mean, group = scenario, fill = scenario)) +
+  geom_ribbon(aes(ymin = lower_CI, ymax = upper_CI, fill = scenario), alpha = 0.25, show.legend = F) +
+  geom_line(aes(colour = scenario), show.legend = T) +
+  xlab("") +
+  scale_y_continuous("HIV tests (millions)", labels = (function(l) {round(l/1e6,1)})) +
+  expand_limits(y=0) + theme_classic() + 
+  theme(axis.text = element_text(size = 14), 
+        axis.title.y = element_text(size = 14), 
+        axis.title.x = element_text(size = 14)) +
+  facet_wrap(vars(test_reduction))
+
+h <- mrcq$enqueue(packageVersion("ggplot2"))
+h$wait(10)
---
+++ THEMBISAv18/support_modify_inputs_cluster.R
@@ -0,0 +1,63 @@
+
+create_data_dictionary <- function(data) {
+  ## Split each line into a vector, using tabs as separators
+  data <- strsplit(data, "\t")
+  ## Grab first element of each line so we can test if it is data or text
+  first_element <- sapply(data, "[", 1)
+  ## Identify the lines that contain numerical data
+  idx_data <- grepl("^[0-9]", first_element)
+
+  # Extract descriptions of data, which are stored in first element of vector
+  descriptions <- first_element[!idx_data]
+
+  ## Identify which descriptions refer to which data (some are repeated)
+  ## This gives a vector the same length as the data with entries indicating
+  ## which description is used for that data
+  idx_description <- cumsum(!idx_data)[idx_data]
+
+  ## Output a data dictionary, giving the line numbers in the text file referring to
+  ## - line number of the data itself
+  ## - line number of the description of the data
+  ## - name of data - defaults to X1, ..., Xn
+  ## - text string describing the data
+  dictionary <- data.frame(line_data = which(idx_data),
+                           line_description = which(!idx_data)[idx_description],
+                           name = paste0("X", seq_len(sum(idx_data))),
+                           description = descriptions[idx_description])
+  dictionary
+}
+
+
+save_dictionary <- function(dictionary, file) {
+  write.csv(rollout_dictionary, file, row.names = FALSE)
+  
+}
+
+## Convert Thembisa inputs into easily modifiable format
+format_data <- function(data, dictionary, start_year = 1985, end_year = 2100) {
+  ## Split each line into a vector, using tabs as separators
+  data <- strsplit(data, "\t")
+  ## Convert data from characters to numbers
+  x <- as.numeric(unlist(data[dictionary$line_data]))
+  ## Arrange in a matrix with one column per data set
+  ret <- matrix(x, ncol = nrow(dictionary))
+  colnames(ret) <- dictionary$name
+
+  ## Add column indicating year of data
+  year <- seq(start_year, end_year)
+  if (length(year) != nrow(ret)) {
+    stop("number of data entries does not match year range provided")
+  }
+  list(year = year,
+       data = ret)
+}
+
+
+
+convert_to_thembisa_format <- function(formatted_data, data, dictionary) {
+  values <- apply(formatted_data$data, MARGIN = 2,
+                  function(x) paste(sprintf("%.4f", x), collapse = "\t"))
+  ret <- replace(data, list = dictionary$line_data, values)
+  ret
+}
+

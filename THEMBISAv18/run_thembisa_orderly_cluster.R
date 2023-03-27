8ca39ed4d78ffdff7c97a6b0759b1366923edf3e
Stefan Rautenbach <48711765+StefanRautenbach@users.noreply.github.com>
2023-03-27 10:04:41 +0100

    updated condom use
    Changed produced results using the change on the odds scale and converted back to probability
    
    Increased the resolution for condom promotion (not too sure about the reduction)

--- THEMBISAv18/run_thembisa_orderly_cluster.R
+++ THEMBISAv18/run_thembisa_orderly_cluster.R
@@ -2,51 +2,55 @@ library(orderly)
 library(dplyr)
 library(tidyr)
 library(purrr)
+library(parallel)
 
 # check that thembise.exe is up to date
-# setwd("Q:/Git/HIV_EndGame_SA/THEMBISAv18")
-# system("compile.bat")
-# system("./thembisa.exe")
+setwd("M:/HIV_EndGame_SA/THEMBISAv18")
+system("compile.bat")
+system("./thembisa.exe")
 
 # copy this version of thembisa.exe into the orderly src
 
 # move into orderly directory and check
-workdir <- "Q:/Git/HIV_EndGame_SA/orderly/thembisa_orderly/src/thembisa"
+workdir <- "M:/HIV_EndGame_SA/orderly/thembisa_orderly/src/thembisa"
 setwd(workdir)
 getwd()
 
-# check that ordely works where you are
+# check that orderly works where you are
 orderly::orderly_run(parameters = list(pitc_reduction_years = 2025,
-                     pitc_reduction_percentage = 0,
-                     condom_usage_reduction = FALSE,
-                     condom_usage_decrease = 0,
-                     condom_decr_start = 2020,
-                     condom_usage_promotion = TRUE,
-                     condom_usage_increase = 14,
-                     condom_incr_start = 2025,
-                     art_coverage_increase = FALSE,
-                     art_interrupt_rate_decrease = 0,
-                     art_incr_start= 2020,
-                     art_coverage_decrease = FALSE,
-                     art_interrupt_rate_increase = 2020,
-                     art_decr_start = 0,
-                     cumulative_years = 50,
-                     summary_name = "summary"))
+                                       pitc_reduction_percentage = 0,
+                                       condom_usage_reduction = FALSE,
+                                       condom_usage_decrease = 0,
+                                       condom_decr_start = 2020,
+                                       condom_usage_promotion = TRUE,
+                                       condom_incr_start = 2025,
+                                       condom_usage_increase = 1,
+                                       art_coverage_increase = FALSE,
+                                       art_interrupt_rate_decrease = 0,
+                                       art_incr_start= 2020,
+                                       art_coverage_decrease = FALSE,
+                                       art_interrupt_rate_increase = 2020,
+                                       art_decr_start = 0,
+                                       cumulative_years = 50,
+                                       summary_name = "summary"))
 
 
 task <- "thembisa"
-root <- "Q:/Git/HIV_EndGame_SA/orderly/thembisa_orderly/contexts"
-orderly_root <- "Q:/Git/HIV_EndGame_SA/orderly/thembisa_orderly"
+root <- "M:/HIV_EndGame_SA/orderly/thembisa_orderly/contexts"
+orderly_root <- "M:/HIV_EndGame_SA/orderly/thembisa_orderly"
 path_bundles <- file.path(orderly_root, "bundles")
 output_path <- "output"
 
 # parameter values - change for each run
 
-pitc_reduction_years<- seq(2025, 2050, 5)
-pitc_reduction_percentage <- seq(0,100,10) 
+pitc_reduction_years<- c(2025)
+pitc_reduction_percentage <- seq(0,100,5)
 condom_usage_reduction <- FALSE
 condom_usage_decrease <- 0
 condom_incr_start <- 2025
+condom_usage_promotion <- TRUE
+condom_usage_increase <- seq(0, 14, 0.5)
+condom_decr_start <- 2025
 art_coverage_increase <- FALSE
 art_interrupt_rate_decrease <- 0 
 art_incr_start <- 2025
@@ -62,7 +66,10 @@ pars <- expand_grid(pitc_reduction_years,
                     pitc_reduction_percentage, 
                     condom_usage_reduction,
                     condom_usage_decrease,
-                    condom_incr_start, 
+                    condom_decr_start,
+                    condom_usage_promotion,
+                    condom_incr_start,
+                    condom_usage_increase,
                     art_coverage_increase,
                     art_interrupt_rate_decrease,
                     art_incr_start,
@@ -81,6 +88,9 @@ bundle <- lapply(X = seq_len(nrow(pars)), FUN = function(i){
                       pitc_reduction_percentage = pars$pitc_reduction_percentage[i],
                       condom_usage_reduction = pars$condom_usage_reduction[i],
                       condom_usage_decrease = pars$condom_usage_decrease[i],
+                      condom_decr_start = pars$condom_decr_start[i],
+                      condom_usage_promotion = pars$condom_usage_promotion[i],
+                      condom_usage_increase = pars$condom_usage_increase[i],
                       condom_incr_start = pars$condom_incr_start[i],
                       art_coverage_increase = pars$art_coverage_increase[i],
                       art_interrupt_rate_decrease = pars$art_interrupt_rate_decrease[i],
@@ -95,7 +105,8 @@ bundle <- lapply(X = seq_len(nrow(pars)), FUN = function(i){
 
 
 # set up config, packages, sources, creating context and making queue object
-config <- didehpc::didehpc_config(credentials = "spr21",cluster = "small")
+share <- didehpc::path_mapping("Stefan", "M:", "//wpia-hn.hpc.dide.ic.ac.uk/hiv-inference/Stefan", "M:")
+config <- didehpc::didehpc_config(credentials = "spr21",template = "AllNodes", cluster = "wpia-hn", shares = share, cores = 1)
 packages = c("tidyr", "dplyr","ggplot2", "readr", "purrr", "orderly")
 sources <- c("R/modify_rollout_orderly.R", "R/read_and_run_orderly.R", "R/support_modify_inputs_orderly.R", "R/cluster_function_orderly.R")
 ctx <- context::context_save(path = root, packages = packages, sources = sources)
@@ -123,13 +134,14 @@ batch_name <- t$name
 obj$task_bundle_get(batch_name)$results()
 # status
 t$status()
+which(t$status()=="ERROR")
 # look at logs of tasks
 tasks <- t$tasks
 # check log of specific tasks - task 1 below
-tasks[[1]]$log()
-
+tasks[[4]]$log()
+t$status()
 # import to archive
-for (output in t$wait(100)) {
+for (output in t$wait(100)[]) {
   out <- strsplit(output$path, "\\\\")[[1]]
   output_filename <- out[length(out)]
   orderly::orderly_bundle_import(file.path(workdir, output_path, output_filename),
@@ -138,13 +150,16 @@ for (output in t$wait(100)) {
 
 # find all the most recent tasks in archive that meet criteria in parameters
 # can change pars to include multiple future variability 
-
+t1 <- Sys.time()
 archive_task_names <- unlist(lapply(seq_len(nrow(pars)), function(i) {
   orderly::orderly_search(name = "thembisa", 
                           query = "latest(parameter:pitc_reduction_years == pitc_reduction_years && 
                           parameter:pitc_reduction_percentage == pitc_reduction_percentage &&  
                           parameter:condom_usage_reduction == condom_usage_reduction &&
                           parameter:condom_usage_decrease == condom_usage_decrease &&
+                          parameter:condom_decr_start == condom_incr_start &&
+                          parameter:condom_usage_promotion == condom_usage_promotion &&
+                          parameter:condom_usage_increase == condom_usage_increase &&
                           parameter:condom_incr_start == condom_incr_start &&
                           parameter:art_coverage_increase == art_coverage_increase &&
                           parameter:art_interrupt_rate_decrease == art_interrupt_rate_decrease &&
@@ -158,6 +173,9 @@ archive_task_names <- unlist(lapply(seq_len(nrow(pars)), function(i) {
                                             pitc_reduction_percentage = pars$pitc_reduction_percentage[i],
                                             condom_usage_reduction = pars$condom_usage_reduction[i],
                                             condom_usage_decrease = pars$condom_usage_decrease[i],
+                                            condom_decr_start = pars$condom_incr_start[i],
+                                            condom_usage_promotion = pars$condom_usage_promotion[i],
+                                            condom_usage_increase = pars$condom_usage_increase[i],
                                             condom_incr_start = pars$condom_incr_start[i],
                                             art_coverage_increase = pars$art_coverage_increase[i],
                                             art_interrupt_rate_decrease = pars$art_interrupt_rate_decrease[i],
@@ -168,10 +186,13 @@ archive_task_names <- unlist(lapply(seq_len(nrow(pars)), function(i) {
                                             cumulative_years = pars$cumulative_years[i],
                                             summary_name = pars$summary_name[i])
   )}))
+t2 <- Sys.time()
+t3 <- t2 - t1
+
 sum(is.na(archive_task_names))
 archive_task_names <- archive_task_names[which(!is.na(archive_task_names))]
 # combine the summary csvs
-filepaths <- paste0("Q:/Git/HIV_EndGame_SA/orderly/thembisa_orderly/archive/thembisa/", archive_task_names, "/results/summary.csv")
+filepaths <- paste0("M:/HIV_EndGame_SA/orderly/thembisa_orderly/archive/thembisa/", archive_task_names, "/results/summary.csv")
 temp <- lapply(filepaths, read.csv)
 names(temp) <- archive_task_names  
 combined_summary <- bind_rows(temp, .id = "task_name")
@@ -183,7 +204,7 @@ csv_name <- unique(combined_summary$future_variability)
 inc_and_elim <- find_inc_and_elimination(combined_summary)
 
 # combine the cumulative csvs
-filepaths <- paste0("Q:/Git/HIV_EndGame_SA/orderly/thembisa_orderly/archive/thembisa/", archive_task_names, "/results/cumulative_summary.csv")
+filepaths <- paste0("M:/HIV_EndGame_SA/orderly/thembisa_orderly/archive/thembisa/", archive_task_names, "/results/cumulative_summary.csv")
 temp <- lapply(filepaths, read.csv)
 names(temp) <- archive_task_names  
 combined_cumulative <- bind_rows(temp, .id = "task_name")
